d500cfe484d4bef772100a50ace92d6a7cbb8217
Hadi Farnoud <hadi.farnoud@gmail.com>
2017-12-09 22:07:51 +0330

    remove sandbox and strip plugins

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const prerender = require('prerender');
 
 const forwardHeaders = require('./plugins/forwardHeaders');
-const stripHtml = require('./plugins/stripHtml');
-const healthcheck = require('./plugins/healthcheck');
 const removePrefetchTags = require('./plugins/removePrefetchTags');
 const log = require('./plugins/log');
 
@@ -10,23 +8,20 @@ const options = {
 	pageDoneCheckInterval : 500,
 	pageLoadTimeout: 20000,
 	waitAfterLastRequest: 250,
-	chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
+	chromeFlags: [ '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
 };
 console.log('Starting with options:', options);
 
 const server = prerender(options);
 
 server.use(log);
-server.use(healthcheck('_health'));
 server.use(forwardHeaders);
 server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(removePrefetchTags);
-// server.use(require('prerender-redis-cache'));
 server.use(prerender.httpHeaders());
 if (process.env.DEBUG_PAGES) {
 	server.use(prerender.logger());
 }
-server.use(stripHtml);
 
 server.start();
