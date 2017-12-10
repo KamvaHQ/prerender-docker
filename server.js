089f1178a1cd4a5f6c8822b388a3b151cd322421
Hadi Farnoud <hadi.farnoud@gmail.com>
2017-12-10 09:27:51 +0330

    add pm2 back and just make instance=1. also removed html strip plugin

--- server.js
+++ server.js
@@ -8,17 +8,19 @@ const options = {
 	pageDoneCheckInterval : 500,
 	pageLoadTimeout: 20000,
 	waitAfterLastRequest: 250,
-	chromeFlags: [ '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
+	chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
 };
 console.log('Starting with options:', options);
 
 const server = prerender(options);
 
 server.use(log);
+server.use(healthcheck('_health'));
 server.use(forwardHeaders);
 server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(removePrefetchTags);
+// server.use(require('prerender-redis-cache'));
 server.use(prerender.httpHeaders());
 if (process.env.DEBUG_PAGES) {
 	server.use(prerender.logger());
